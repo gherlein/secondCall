4634813233aed6ef32e999976bbb016aeaa075c8
Greg Herlein <gherlein@herlein.com>
2021-10-10 17:23:48 +0000

    touch up

--- src/secondCall.js
+++ src/secondCall.js
@@ -3,32 +3,32 @@ const AWS = require('aws-sdk');
 const wavFileBucket = process.env['WAVFILE_BUCKET']
 
 exports.handler = async(event, context, callback) => {
-    console.log("Lambda is invoked with calldetails:" + JSON.stringify(event));
+    console.log(JSON.stringify(event));
     let actions;
 
     switch (event.InvocationEventType) {
         case "NEW_INBOUND_CALL":
-            console.log("INBOUND " + JSON.stringify(event));
+//            console.log("NEW_INBOUND_CALL");
             actions = await newCall(event);
             break;
 
         case "ACTION_SUCCESSFUL":
-            console.log("SUCCESS ACTION");
+//            console.log("SUCCESS ACTION");
             actions = [hangupAction];
 	    break;
         
         case "HANGUP":
-            console.log("HANGUP ACTION");
+//            console.log("HANGUP ACTION");
             actions = []
             break;
 
         case "CALL_ANSWERED":
-            console.log("CALL ANSWERED")
+//            console.log("CALL ANSWERED")
             actions = []
             break;
 
         default:
-            console.log("FAILED ACTION");
+//            console.log("FAILED ACTION");
             actions = [hangupAction];
     }
 
@@ -37,12 +37,10 @@ exports.handler = async(event, context, callback) => {
         "Actions": actions
     };
 
-    console.log("Sending callback response:" + JSON.stringify(response));
     callback(null, response);
 }
 
 async function newCall(event, details) {
-    console.log("got call from" + event.CallDetails.Participants[0].From)
     playAudioAction.Parameters.AudioSource.Key = "hello-goodbye.wav";
     return [playAudioAction,hangupAction];
 }
